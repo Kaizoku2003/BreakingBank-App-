c1bca8477626b49a43678687abf198164d11cd9b
Jesús Centurión <jdcenturion1@fpuna.edu.py>
2025-11-22 18:55:32 -0300

    Primera subida de archivos

---
+++ src/breakingbank/BreakingBank.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package breakingbank;
+
+/**
+ *
+ * @author Jesús Centurión 
+ * @author Fabrizio Falcón 
+ * @author Santino Gianninotto
+ * @author Benjamín Ojeda 
+ */
+public class BreakingBank {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+        new inicio().setVisible(true);  
+    });
+    }
+    
+}

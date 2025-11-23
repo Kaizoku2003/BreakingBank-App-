a01bf83acb1272af24221ba38ade95ff27b9dd14
benja <benja@Benjamin>
2025-11-23 15:22:27 -0300

    CREAR CLASE USUARIO , REPOSITORY {manejo de archivos txt, etc} Y AUTHSERVICE {manejo de logica, verificacion de contrasenas, cedula, etc}, en teoria ya funciona el login y el registro y god.. se actualizo la clase usuario para agregar saldo como atributo, lo mismo para repository y authservice, se creo la clase cuentaservice, para el manejo de los saldos, sumas y demas (todos abiertos a modificaciones para los casos de pago de servicios y transferencias) , en teoria ya estan funcionando tambien las clases de deposito y saldo, actualizando el saldo para cada usuario y guardado en txt

---
+++ src/breakingbank/authService.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package breakingbank;
+
+/**
+ *
+ * @author benja
+ */
+
+
+
+
+public class authService {
+
+    private final UsuarioRepository repo = new UsuarioRepository();
+
+    // usuario actual logueado (compartido para toda la app)
+    private static Usuario usuarioActual = null;
+
+    public boolean registrarUsuario(String nombre, String telefono, String correo,
+                                    String direccion, String cedula, String password) {
+
+        if (repo.existeCedula(cedula)) {
+            return false; // ya existe
+        }
+
+        Usuario u = new Usuario(nombre, telefono, correo, direccion, cedula, password, 0.0);
+        return repo.guardar(u);
+    }
+
+    public Usuario iniciarSesion(String nombre, String password) {
+        Usuario u = repo.buscarPorNombreYPassword(nombre, password);
+        if (u != null) {
+            usuarioActual = u;
+        }
+        return u;
+    }
+
+    public static Usuario getUsuarioActual() {
+        return usuarioActual;
+    }
+
+    public static void cerrarSesion() {
+        usuarioActual = null;
+    }
+}
